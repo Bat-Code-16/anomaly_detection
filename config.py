274da5f9063d099ae9cae5390518547de5ed519b
parvesh-verma-16 <parvesh.verma@bitcomm.co.in>
2024-08-09 20:45:04 +0530

    third commit

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import psycopg2
 import pandas as pd
+from datetime import datetime
 
 db_params1 = {     # ele database connection
     "dbname": "edge",
@@ -9,7 +10,47 @@ db_params1 = {     # ele database connection
     "port": "5432"
 }
 
+def date_to_ts(start_date,end_date):
+    ts=(start_date.timestamp())*1000,
+    te=(end_date.timestamp())*1000,
+    return ts, te
+
 def get_data(cursor, ts, tt):
+    # get_data_sql = """
+    # SELECT 
+    #     h.ts,
+    #     h.deviceid,
+    #     h."key",
+    #     h.long_v
+    # FROM 
+    #     public.history h
+    # JOIN 
+    #     public.device d ON h.deviceid = d.deviceid 
+    # WHERE 
+    #     d.typeid = '69a18cc9-88d4-4b9a-87ac-4f58fceec109'
+    #     AND h."key" IN ('OC', 'OT', 'PC') 
+    #     AND h.ts BETWEEN %s AND %s
+    # ORDER BY 
+    #     h.ts ASC
+    # LIMIT 100;
+    # """
+    # get_data_sql = """
+    # SELECT 
+    #     h.ts,
+    #     h.deviceid,
+    #     h."key",
+    #     h.long_v
+    # FROM 
+    #     public.history h
+    # JOIN 
+    #     public.device d ON h.deviceid = d.deviceid 
+    # WHERE 
+    #     d.typeid = '69a18cc9-88d4-4b9a-87ac-4f58fceec109' AND d.customer_id='50bc6230-3efb-11ed-818c-150801d2970c'
+    #     AND h."key" IN ('OC', 'OT', 'PC') 
+    #     AND h.ts BETWEEN %s AND %s
+    # ORDER BY 
+    #     h.ts ASC;
+    # """
     get_data_sql = """
     SELECT 
         h.ts,
@@ -21,8 +62,8 @@ def get_data(cursor, ts, tt):
     JOIN 
         public.device d ON h.deviceid = d.deviceid 
     WHERE 
-        d.typeid = '69a18cc9-88d4-4b9a-87ac-4f58fceec109' 
-        AND h."key" IN ('OC', 'OT', 'PC') 
+        d.typeid = '5e2684a1-0725-415f-9cd3-3b8159cdda45' AND d.customer_id='50bc6230-3efb-11ed-818c-150801d2970c'
+        AND h."key" IN ('VR', 'IR', 'IF') 
         AND h.ts BETWEEN %s AND %s
     ORDER BY 
         h.ts ASC;
@@ -43,18 +84,18 @@ def get_data(cursor, ts, tt):
 
 def main():
     conn1 = cursor1 = None
-    ts = 1719772200000  # Example start timestamp
-    tt = 1722450600000  # Example end timestamp
-
+    start_date=datetime(2024, 6, 1)
+    end_date=datetime(2024, 8, 1)
     try:
         conn1 = psycopg2.connect(**db_params1)
         cursor1 = conn1.cursor()
         print("ele connected")
-
+        
+        ts,tt=date_to_ts(start_date,end_date)
         # Fetch the data for the specified time range
         df = get_data(cursor1, ts, tt)
 
-        csv_file_path = "C:/Users/HP/Downloads/output_data.csv"
+        csv_file_path = "C:/Users/HP/Downloads/output_data1.csv"
 
         df.to_csv(csv_file_path, index=False)  # Save DataFrame to CSV
 
